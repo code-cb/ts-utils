1e30b1b8b232377b9d634415c286a6e4e5ae421b
ICodeMyOwnLife <icodemyownlife@gmail.com>
2022-10-06 10:03:16 +0700

    Add AnyDict

--- src/object/Dict.ts
+++ src/object/Dict.ts
@@ -1,3 +1,5 @@
 export type Dict<Value = string, Key extends PropertyKey = string> = {
   [K in Key]: Value;
 };
+
+export type AnyDict = Dict<any>;
--- test/object/Dict.ts
+++ test/object/Dict.ts
@@ -1,20 +1,32 @@
-import { id } from 'common';
+import { assert, Equals, id } from 'common';
 import * as O from 'object';
 
 describe(`O.Dict`, () => {
   it(`works`, () => {
+    assert<Equals<O.Dict<number>, { [K: string]: number }>>();
     id<O.Dict<number>>({ a: 1, b: 2 });
     id<O.Dict<number>>({
       a: 1,
       // @ts-expect-error
       b: '2',
     });
+
+    assert<Equals<O.Dict, { [K: string]: string }>>();
     id<O.Dict>({ a: '1', b: '2' });
+
+    assert<Equals<O.Dict<boolean, symbol>, { [K: symbol]: boolean }>>();
     id<O.Dict<boolean, symbol>>({
       [Symbol('a')]: true,
       [Symbol('b')]: false,
       // @ts-expect-error
       a: false,
     });
+  });
+});
+
+describe(`O.AnyDict`, () => {
+  it(`works`, () => {
+    assert<Equals<O.AnyDict, { [K: string]: any }>>();
+    id<O.AnyDict>({ a: 1, b: '2', c: true, d: () => {}, e: /abc/ });
   });
 });
