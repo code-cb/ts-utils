67dca2fee7a9968335f28658af720572f428ca29
ICodeMyOwnLife <icodemyownlife@gmail.com>
2023-01-04 17:37:11 +0700

    Fix Objectify type

--- src/object/fromEntries.ts
+++ src/object/fromEntries.ts
@@ -5,9 +5,19 @@
  * - https://github.com/sindresorhus/ts-extras/blob/2874930a648d7d7cdd6c742da2ec091670144f04/source/object-from-entries.ts
  */
 
-export type Objectify<Union extends readonly [PropertyKey, unknown]> = {
+export type UnionizeDeep<Union extends readonly [PropertyKey, unknown]> = {
+  [U in Union as string]: { [K in U[0]]: [K, U[1]] }[U[0]];
+}[string];
+
+type ObjectifyImpl<Union extends readonly [PropertyKey, unknown]> = {
   [K in Union[0]]: Extract<Union, { [0]: K }>[1];
 };
+
+export type Objectify<Union extends readonly [PropertyKey, unknown]> =
+  ObjectifyImpl<
+    // @ts-ignore
+    UnionizeDeep<Union>
+  >;
 
 export const fromEntries = <Entry extends readonly [PropertyKey, unknown]>(
   entries: ReadonlyArray<Entry>,
--- test/object/fromEntries.ts
+++ test/object/fromEntries.ts
@@ -1,5 +1,35 @@
 import { assert, Equals, id } from 'common';
 import * as O from 'object';
+import { UnionizeDeep } from 'object/fromEntries';
+
+describe(`UnionizeDeep`, () => {
+  assert<
+    Equals<
+      UnionizeDeep<['a' | 'b' | 'c', Date]>,
+      ['a', Date] | ['b', Date] | ['c', Date]
+    >
+  >();
+
+  assert<
+    Equals<
+      UnionizeDeep<['a', Date] | ['b', RegExp] | ['c', Function | undefined]>,
+      ['a', Date] | ['b', RegExp] | ['c', Function | undefined]
+    >
+  >();
+
+  assert<
+    Equals<
+      UnionizeDeep<
+        ['a' | 'b', Date] | ['b' | 'c', RegExp] | ['c', Function | undefined]
+      >,
+      | ['a', Date]
+      | ['b', Date]
+      | ['b', RegExp]
+      | ['c', RegExp]
+      | ['c', Function | undefined]
+    >
+  >();
+});
 
 describe(`O.Objectify and O.fromEntries`, () => {
   it(`works`, () => {
@@ -10,16 +40,41 @@ describe(`O.Objectify and O.fromEntries`, () => {
 
     assert<
       Equals<
+        O.Objectify<['a' | 'b' | 'c', Date]>,
+        { a: Date; b: Date; c: Date }
+      >
+    >();
+    const f2 = O.fromEntries(
+      (['a', 'b', 'c'] as const).map(k => [k, new Date()]),
+    );
+    assert<Equals<typeof f2, { a: Date; b: Date; c: Date }>>();
+    expect(f2).toEqual({
+      a: expect.any(Date),
+      b: expect.any(Date),
+      c: expect.any(Date),
+    });
+
+    assert<
+      Equals<
         O.Objectify<['a', number] | ['b', () => Promise<number>]>,
         { a: number; b: () => Promise<number> }
       >
     >();
-    const f2 = O.fromEntries([
+    const f3 = O.fromEntries([
       ['a' as const, 1],
       ['b' as const, () => Promise.resolve(2)],
     ]);
-    assert<Equals<typeof f2, { a: number; b: () => Promise<number> }>>();
-    expect(f2).toEqual({ a: 1, b: expect.any(Function) });
+    assert<Equals<typeof f3, { a: number; b: () => Promise<number> }>>();
+    expect(f3).toEqual({ a: 1, b: expect.any(Function) });
+
+    assert<
+      Equals<
+        O.Objectify<
+          ['a' | 'b', Date] | ['b' | 'c', RegExp] | ['c', Function | undefined]
+        >,
+        { a: Date; b: Date | RegExp; c: RegExp | Function | undefined }
+      >
+    >();
 
     enum Mode {
       Dark,
@@ -32,10 +87,10 @@ describe(`O.Objectify and O.fromEntries`, () => {
       ['type', id<NodeType>('text')] as const,
       [Symbol.iterator, () => ({})] as const,
     ];
-    const f3 = O.fromEntries(entries);
+    const f4 = O.fromEntries(entries);
     assert<
       Equals<
-        typeof f3,
+        typeof f4,
         {
           mode: Mode;
           1: () => Promise<number>;
@@ -44,11 +99,11 @@ describe(`O.Objectify and O.fromEntries`, () => {
         }
       >
     >();
-    expect(f3).toMatchObject({
+    expect(f4).toMatchObject({
       mode: Mode.Dark,
       1: expect.any(Function),
       type: 'text',
     });
-    expect(f3[Symbol.iterator]).toEqual(expect.any(Function));
+    expect(f4[Symbol.iterator]).toEqual(expect.any(Function));
   });
 });
